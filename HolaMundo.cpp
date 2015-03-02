5966247ef4f4b85dbcbe2179f4764dbb1a4e4fda
Enrique Lazcorreta Puigmartí <enrique.lazcorreta@gmail.com>
2015-03-02 18:37:50 +0100

    Segundo commit de esta aplicación, todavía probando el git

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -1,6 +1,16 @@
+//
+//  main.cpp
+//  HolaMundoDesdeXCode
+//
+//  Created by Enrique Lazcorreta Puigmartí on 24/2/15.
+//  Copyright (c) 2015 Enrique Lazcorreta Puigmartí. All rights reserved.
+//
+
 #include <iostream>
 
-int main()
+#include <stdlib.h>
+
+int main(int argc, const char *argv[])
 {
   std::cout << "Hola, mundo" << std::endl;
   
